730bdbe64679304970d0b3c20306758eedddb130
pearz <1095971161@qq.com>
2022-03-16 10:11:49 +0800

    Initial commit MyBatis配置完毕

---
+++ src/main/java/com/example/emos/wx/EmosWxApiApplication.java
@@ -0,0 +1,16 @@
+package com.example.emos.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pearz
+ */
+@SpringBootApplication
+public class EmosWxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmosWxApiApplication.class, args);
+    }
+
+}
