13c1da00e34ace1d73cc93f9008b3af72e131b85
pearz <1095971161@qq.com>
2022-03-17 19:00:30 +0800

    继承HttpServletRequestWrapper，以抵御跨站脚本攻击

--- src/main/java/com/example/emos/wx/EmosWxApiApplication.java
+++ src/main/java/com/example/emos/wx/EmosWxApiApplication.java
@@ -2,11 +2,13 @@ package com.example.emos.wx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author pearz
  */
 @SpringBootApplication
+@ServletComponentScan
 public class EmosWxApiApplication {
 
     public static void main(String[] args) {
