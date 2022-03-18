93eb3ce88ad1b5afb7aaf9919fe564795b4a6889
pearz <1095971161@qq.com>
2022-03-18 11:20:14 +0800

    把令牌封装成认证对象

---
+++ src/main/java/com/example/emos/wx/config/shiro/OAuth2Token.java
@@ -0,0 +1,28 @@
+package com.example.emos.wx.config.shiro;
+
+import org.apache.shiro.authc.AuthenticationToken;
+
+/**
+ * @Description
+ * @Author pearz
+ * @Email zhaihonghao317@163.com
+ * @Date 11:18 2022/3/18
+ */
+public class OAuth2Token implements AuthenticationToken {
+
+    private String token;
+
+    public OAuth2Token(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return token;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+}
